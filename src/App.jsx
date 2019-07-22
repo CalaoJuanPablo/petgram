4ee983264cae04953d63b087e0192704bcc4c937
Juan Pablo Calao Perez <juan.calao@ideaware.co>
2019-07-22 16:06:51 -0500

    set initial env

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App (props) {
+  return (
+    <React.Fragment>
+      <h1>Hola Petgram</h1>
+    </React.Fragment>
+  )
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  output: {
+    filename: 'app.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ]
+}
