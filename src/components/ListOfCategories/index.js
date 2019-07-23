530f82f27a6e97affe32ac20cbbc5e816bf59775
Juan Pablo Calao Perez <juan.calao@ideaware.co>
2019-07-23 17:43:45 -0500

    categories from api

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Category } from '../Category'
 import { List, Item } from './styles'
-import { categories } from '../../../api/db.json'
 
-export const ListOfCategories = () => (
-  <List>
-    {
-      categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
-    }
-  </List>
-)
+export const ListOfCategories = () => {
+  const [categories, setCategories] = useState([])
+
+  useEffect(function () {
+    window.fetch('https://petgram-server-cjp.calaojuanpablo.now.sh/categories')
+      .then(res => res.json())
+      .then(data => {
+        setCategories(data)
+      })
+  }, [])
+
+  return (
+    <List>
+      {
+        categories.map(
+          category =>
+            <Item key={category.id}><Category {...category} /></Item>
+        )
+      }
+    </List>
+  )
+}
