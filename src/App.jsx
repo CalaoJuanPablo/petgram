d6f339575f2310b6c1e7e06e3f57101da7c62c4c
Juan Pablo Calao Perez <juan.calao@ideaware.co>
2019-07-23 17:10:47 -0500

    add logo

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import { GlobalStyles } from './GlobalStyles'
 import { ListOfCategories } from './components/ListOfCategories'
 import { PhotoCard } from './components/PhotoCard'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards'
+import { Logo } from './components/Logo'
 
 function App (props) {
   return (
     <React.Fragment>
+      <Logo />
       <GlobalStyles />
       <ListOfCategories />
       <PhotoCard />
