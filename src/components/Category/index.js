75a1ff11495ecf8a145e78207ef7405e8a4c185b
Juan Pablo Calao Perez <juan.calao@ideaware.co>
2019-07-26 12:16:33 -0500

    placeholder loading images

--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Anchor, Image } from './styles'
 
-const DEFAULT_IMAGE = 'https://i.imgur.com/dJa0Hpl.jpg'
+const DEFAULT_IMAGE = 'https://www.polytec.com.au/img/products/960-960/oyster-grey.jpg'
 
 export const Category = ({ cover = DEFAULT_IMAGE, path, emoji = '?' }) => (
   <Anchor href={path}>
--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -2,25 +2,60 @@ import React, { useState, useEffect } from 'react'
 import { Category } from '../Category'
 import { List, Item } from './styles'
 
-export const ListOfCategories = () => {
+function useCategoriesData () {
   const [categories, setCategories] = useState([])
+  const [loading, setLoading] = useState(false)
 
   useEffect(function () {
+    setLoading(true)
     window.fetch('https://petgram-server-cjp.calaojuanpablo.now.sh/categories')
       .then(res => res.json())
       .then(data => {
         setCategories(data)
+        setLoading(false)
       })
   }, [])
 
-  return (
-    <List>
+  return { categories, loading }
+}
+
+export const ListOfCategories = () => {
+  const [showFixed, setShowFixed] = useState(false)
+  const { categories, loading } = useCategoriesData()
+
+  useEffect(function () {
+    function onScroll (e) {
+      window.scrollY > 200
+        ? setShowFixed(true)
+        : setShowFixed(false)
+    }
+
+    document.addEventListener('scroll', onScroll)
+
+    return function () {
+      document.removeEventListener('scroll', onScroll)
+    }
+  }, [showFixed])
+
+  const renderList = (fixed) =>
+    <List fixed={fixed}>
       {
-        categories.map(
-          category =>
-            <Item key={category.id}><Category {...category} /></Item>
-        )
+        loading
+          ? [1, 2, 3, 4, 5].map(
+            placeholder =>
+              <Item key={placeholder}><Category /></Item>
+          )
+          : categories.map(
+            category =>
+              <Item key={category.id}><Category {...category} /></Item>
+          )
       }
     </List>
+
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(showFixed)}
+    </>
   )
 }
