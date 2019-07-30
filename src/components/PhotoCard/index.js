473e2380c2fc0988ff150b05b7fad0b45e2e5973
Juan Pablo Calao Perez <juan.calao@ideaware.co>
2019-07-30 09:12:49 -0500

    polyfill IntersectionObserver

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -9,16 +9,22 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   const articelRef = useRef(null)
 
   useEffect(function () {
-    const observer = new window.IntersectionObserver(function (entries) {
-      const { isIntersecting } = entries[0]
+    Promise.resolve(
+      typeof window.IntersectionObserver !== 'indefined'
+        ? window.IntersectionObserver
+        : import('intersection-observer')
+    ).then(() => {
+      const observer = new window.IntersectionObserver(function (entries) {
+        const { isIntersecting } = entries[0]
 
-      if (isIntersecting) {
-        setShow(true)
-        observer.disconnect()
-      }
+        if (isIntersecting) {
+          setShow(true)
+          observer.disconnect()
+        }
+      })
+
+      observer.observe(articelRef.current)
     })
-
-    observer.observe(articelRef.current)
   }, [])
 
   return (
