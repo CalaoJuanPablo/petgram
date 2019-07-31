2e727374ba4bf84dd44db36e5db4a3ba7c063138
Juan Pablo Calao Perez <juan.calao@ideaware.co>
2019-07-31 09:24:15 -0500

    set local storage likes

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -4,14 +4,38 @@ import { Article, ImageWrapper, Image, Button } from './styles'
 
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
 
+function useLocalStorage (key, initialVal) {
+  const [storedValue, setStoredValue] = useState(function () {
+    try {
+      const item = window.localStorage.getItem(key)
+      return item !== null ? JSON.parse(item) : initialVal
+    } catch (e) {
+      console.log(e)
+      return initialVal
+    }
+  })
+
+  function setLocalStorage (value) {
+    try {
+      window.localStorage.setItem(key, JSON.stringify(value))
+      setStoredValue(value)
+    } catch {
+      console.log('Error')
+    }
+  }
+
+  return [storedValue, setLocalStorage]
+}
+
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   const [show, setShow] = useState(false)
-  const [liked, setLiked] = useState(false)
   const articelRef = useRef(null)
+  const key = `likes_${id}`
+  const [liked, setLiked] = useLocalStorage(key, false)
 
   useEffect(function () {
     Promise.resolve(
-      typeof window.IntersectionObserver !== 'indefined'
+      typeof window.IntersectionObserver !== 'undefined'
         ? window.IntersectionObserver
         : import('intersection-observer')
     ).then(() => {
